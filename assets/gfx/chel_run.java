eec7e77eb9a8d1c1886d8931f060cd08994f4ec5
Home_PC <sgyxa17@list.ru>
2014-11-16 21:22:21 +0300

    animation sprite

---
+++ assets/gfx/chel_run.java
@@ -0,0 +1,15 @@
+// Definitions for sprite sheet chel_run.png
+// Created with TexturePacker www.codeandweb.com/texturepacker
+
+// $TexturePacker:SmartUpdate:56871ffa3ca72aa4d01cad17fad46324:a96888b5eba23748defcec77c9558e78:317fd131755fcce147ac9a658dc14c53$
+
+package Texture;
+
+public interface chel_run
+{
+	public static final int GTA2_PED_200_ID = 0;
+	public static final int GTA2_PED_203_ID = 1;
+	public static final int GTA2_PED_204_ID = 2;
+	public static final int GTA2_PED_208_ID = 3;
+	public static final int GTA2_PED_211_ID = 4;
+}
---
+++ src/com/sguxa/sampleandengine/AnimationSprite.java
@@ -0,0 +1,5 @@
+package com.sguxa.sampleandengine;
+
+public class AnimationSprite {
+
+}
