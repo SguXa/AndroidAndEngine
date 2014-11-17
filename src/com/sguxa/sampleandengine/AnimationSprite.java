8456473587da58d3a6e8e510b9d052571e547770
SguXa <sgyxa17@list.ru>
2014-11-17 13:23:32 +0300

    Рабочий класс анимации

--- src/com/sguxa/sampleandengine/AnimationSprite.java
+++ src/com/sguxa/sampleandengine/AnimationSprite.java
@@ -10,56 +10,77 @@ import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.opengl.texture.TextureOptions; 
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas; 
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory; 
+import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder;
+import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder.TextureAtlasBuilderException;
 import org.andengine.opengl.texture.region.TiledTextureRegion; 
 import org.andengine.ui.activity.SimpleBaseGameActivity; 
 import org.andengine.util.color.Color;
+import org.andengine.util.debug.Debug;
 
 import android.view.Menu;
 import android.R;
 
 public class AnimationSprite extends SimpleBaseGameActivity {
 	
-	 private BitmapTextureAtlas texCat;
-	 private TiledTextureRegion regCat;
-	 private AnimatedSprite  sprCat;
-	 private Scene m_Scene;
-	 private Camera    m_Camera;
-//	 private static int   SPR_COLUMN  = 2;
-	 private static int   SPR_ROWS  = 5;
-	  
-	 private static final int CAMERA_WIDTH = 480;
-	 private static final int CAMERA_HEIGHT = 320;
-	  
-	 @Override
-	 public EngineOptions onCreateEngineOptions() 
-	 {
-	  m_Camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-	  EngineOptions en = new EngineOptions(true,ScreenOrientation.LANDSCAPE_SENSOR,new RatioResolutionPolicy(CAMERA_WIDTH,CAMERA_HEIGHT),m_Camera);
-	  return en;
-	 }
-	  
-	 @Override
-	 protected void onCreateResources() 
-	 {
-	  texCat = new BitmapTextureAtlas(this.getTextureManager(), 10, 18, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-	  regCat = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(texCat, this,"gfx/chel_run.png", 0, 0, 1, SPR_ROWS);
-	  texCat.load();
-	 }
+	private BuildableBitmapTextureAtlas texCat;
+	private TiledTextureRegion regCat;
+	private AnimatedSprite  sprCat;
+	private Scene m_Scene;
+	private Camera    m_Camera;
 
-	 @Override
-	 protected Scene onCreateScene() 
-	 {
-	  m_Scene = new Scene();
-	  m_Scene.setBackground(new Background(Color.WHITE));
-	  
-	  
-	    
-	  sprCat = new AnimatedSprite(0, 0, regCat, this.getVertexBufferObjectManager());
-	  m_Scene.attachChild(sprCat);
-	    
-	  sprCat.animate(100);
-	  return m_Scene;
-	 }
+	//This represents the sprite sheet(image) rows and columns
+	//We have 4 Rows and 2 Columns 
+	private static int   SPR_COLUMN  = 1;
+	private static int   SPR_ROWS  = 5;
+
+
+	//Set the camera Width and Height 
+	private static final int CAMERA_WIDTH = 480;
+	private static final int CAMERA_HEIGHT = 320;
+
+	//Override the below method from base activity class 
+
+	@Override
+	public EngineOptions onCreateEngineOptions() 
+	{
+	 m_Camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+	 EngineOptions en = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(
+	 CAMERA_WIDTH, CAMERA_HEIGHT), m_Camera);
+	 return en;
+	}
+
+	//Override the below method from base activity class 
+
+	@Override
+	protected void onCreateResources() 
+	{
+		 texCat = new BuildableBitmapTextureAtlas(this.getTextureManager(), 1024, 1024, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		 regCat = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(texCat, this,"gfx/chel_run.png", SPR_COLUMN, SPR_ROWS);
+		 try{
+			 texCat.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource, BitmapTextureAtlas>(0, 0, 1));
+			 texCat.load();
+		 }catch(TextureAtlasBuilderException e){
+			 Debug.e(e);
+		 }
+	}
+
+
+	//Override the below method from base activity class 
+
+	@Override
+	protected Scene onCreateScene() 
+	{
+	 m_Scene = new Scene();
+	 m_Scene.setBackground(new Background(Color.BLACK));
+	   
+	 sprCat = new AnimatedSprite(240, 150, regCat, this.getVertexBufferObjectManager());
+	 m_Scene.attachChild(sprCat);
+	   
+	 sprCat.animate(100);
+	 return m_Scene;
+	}
 	  
 //	 @Override
 //	 public boolean onCreateOptionsMenu(Menu menu) {
