5d11207109c866f599c81cc90621ad602a4692cb
SguXa <sgyxa17@list.ru>
2014-11-17 16:31:15 +0300

    добавление к Анимации, вращение, приближение и т.д.

--- src/com/sguxa/sampleandengine/AnimationSprite.java
+++ src/com/sguxa/sampleandengine/AnimationSprite.java
@@ -1,26 +1,41 @@
 package com.sguxa.sampleandengine;
 
-import org.andengine.engine.camera.Camera; 
-import org.andengine.engine.options.EngineOptions; 
-import org.andengine.engine.options.ScreenOrientation; 
-import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy; 
-import org.andengine.entity.scene.Scene; 
-import org.andengine.entity.scene.background.Background; 
-import org.andengine.entity.sprite.AnimatedSprite; 
-import org.andengine.opengl.texture.TextureOptions; 
-import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas; 
-import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory; 
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.IEntity;
+import org.andengine.entity.modifier.AlphaModifier;
+import org.andengine.entity.modifier.DelayModifier;
+import org.andengine.entity.modifier.IEntityModifier.IEntityModifierListener;
+import org.andengine.entity.modifier.LoopEntityModifier;
+import org.andengine.entity.modifier.LoopEntityModifier.ILoopEntityModifierListener;
+import org.andengine.entity.modifier.ParallelEntityModifier;
+import org.andengine.entity.modifier.RotationByModifier;
+import org.andengine.entity.modifier.RotationModifier;
+import org.andengine.entity.modifier.ScaleModifier;
+import org.andengine.entity.modifier.SequenceEntityModifier;
+import org.andengine.entity.primitive.Rectangle;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
 import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder;
 import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder.TextureAtlasBuilderException;
-import org.andengine.opengl.texture.region.TiledTextureRegion; 
-import org.andengine.ui.activity.SimpleBaseGameActivity; 
+import org.andengine.opengl.texture.region.TiledTextureRegion;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.color.Color;
 import org.andengine.util.debug.Debug;
+import org.andengine.util.modifier.IModifier;
+import org.andengine.util.modifier.LoopModifier;
 
-import android.view.Menu;
-import android.R;
+import android.opengl.GLES20;
+import android.widget.Toast;
 
 public class AnimationSprite extends SimpleBaseGameActivity {
 	
@@ -76,9 +91,36 @@ public class AnimationSprite extends SimpleBaseGameActivity {
 	 m_Scene.setBackground(new Background(Color.BLACK));
 	   
 	 sprCat = new AnimatedSprite(240, 150, regCat, this.getVertexBufferObjectManager());
-	 m_Scene.attachChild(sprCat);
-	   
+//	 m_Scene.attachChild(sprCat);
 	 sprCat.animate(100);
+	 
+	 final LoopEntityModifier entityModifier =
+			 //LoopEntityModifier Ч зацикливает выполнение модификаторов;
+				new LoopEntityModifier(
+						//SequenceEntityModifier Ч выполн€ет модификаторы последовательно, в пор€дке их добавлени€;
+						new SequenceEntityModifier(
+								new RotationModifier(3, 0, 90)//крутить обьект
+//								new AlphaModifier(2, 1, 0)
+//								new AlphaModifier(1, 0, 1)
+//								new ScaleModifier(2, 1, 0.5f),//удал€ет вид от обьекта
+//								new DelayModifier(0.5f)//возвращает к обычному виду
+//ParallelEntityModifier Ч выполн€ет несколько модификаторов одновременно;/
+//								new ParallelEntityModifier(
+//										new ScaleModifier(3, 0.5f, 5),
+//										new RotationByModifier(3, 90)
+//								)
+//								new ParallelEntityModifier(
+//										new ScaleModifier(3, 5, 1),
+//										new RotationModifier(3, 180, 0)
+//								)
+						)
+				);
+
+	 sprCat.registerEntityModifier(entityModifier);
+//	 sprCat.registerEntityModifier(entityModifier.deepCopy());
+
+	 m_Scene.attachChild(sprCat);
+//			scene.attachChild(rect);
 	 return m_Scene;
 	}
 	  
